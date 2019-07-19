ed5598acde6581c996e8e25147e0ea49599cb90b
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-07-19 11:19:24 -0400

    completed NYTimes movie review lab

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,28 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const generateReviews = (reviewsArray) => {
+    return reviewsArray.map(review => {
+        return (
+            <li className='review'>
+                <h3>{review.headline}</h3>
+                <h4>{review.byline}</h4>
+                <p>{review.summary_short}</p>
+            </li>
+        )
+    })
+}
+
+const MovieReviews = (props) => {
+    console.log(props.reviews)
+
+    return (
+        <ul className='review-list' >
+            {generateReviews(props.reviews)}
+        </ul>
+    )
+}
+
+
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,51 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const NYT_API_KEY = 'Ir9Prf4XUEArsOw9SCgsHsbjZPzSGar3';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            reviews: [], 
+            searchTerm: ''
+        }
+    }
+
+    submitSearch = (e) => {
+        console.log('searching...')
+        e.preventDefault()
+        let searchTerm = e.target.children[0].value
+        this.setState({
+            searchTerm: searchTerm
+        })
+        this.fetchReviews(searchTerm)
+    }
+
+    fetchReviews = (searchTerm) => {
+        fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${searchTerm}&api-key=${NYT_API_KEY}`)
+        .then(response => response.json())
+        .then(result => this.setState({
+            reviews: result.results
+        }))
+    }
+
+    render() {
+        return (
+        <div className='searchable-movie-reviews'>
+            <form onSubmit={this.submitSearch}>
+                <input></input>
+                <input type='submit' name='Search'></input>
+            </form>
+            <h1>Reviews related to: {this.state.searchTerm}</h1>
+            <MovieReviews reviews={this.state.reviews} />
+        </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer
